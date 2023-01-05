bbb74cbd99eeb0912c88613cf89089c6a83befd8
Owain Williams <owain@andersdigital.com>
2023-01-05 13:24:05 +0000

    allow engineers to be crud, set up engineer tests

--- engineers.py
+++ engineers.py
@@ -12,8 +12,32 @@ class Engineer:
 
     def setrate(self, rate: float) -> None:
         '''Set the engineer's rate.'''
-        db.execute("UPDATE Engineers SET Rate=%s WHERE EngineerID=%s",
+        cursor.execute("UPDATE Engineers SET Rate=%s WHERE EngineerID=%s",
                    (rate, self.EngineerID))
-        db.commit()
+        cursor.commit()
         self.Rate = rate
         print(f'Engineer {self.FirstName} {self.LastName} rate set to {rate}')
+        
+    def commit(self) -> None:
+        '''Add or update the engineer in the database.'''
+        if self.EngineerID is None:
+            cursor.execute(
+                "INSERT INTO Engineers (FirstName, LastName, Rate) VALUES (%s, %s, %s)",
+                (self.FirstName, self.LastName, self.Rate)
+            )
+            self.EngineerID = cursor.lastrowid
+            print(
+                f'Engineer "{self.FirstName} {self.LastName}" created. ID: {self.EngineerID}')
+        else:
+            cursor.execute(
+                "UPDATE Engineers SET FirstName=%s, LastName=%s, Rate=%s WHERE EngineerID=%s",
+                (self.FirstName, self.LastName, self.Rate, self.EngineerID)
+            )
+            print(
+                f'Engineer "{self.FirstName} {self.LastName}" updated. ID: {self.EngineerID}')
+            
+
+def getallengineers() -> list[Engineer]:
+    '''Get all engineers from the database.'''
+    cursor.execute("SELECT * FROM Engineers")
+    return [Engineer(*engineer) for engineer in cursor.fetchall()]
