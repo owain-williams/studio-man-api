6ca544992c8f8b81c6aa6e5f87db541a1bb1159c
Owain Williams <owain@andersdigital.com>
2023-01-05 12:57:46 +0000

    allow engineer rate setting

--- engineers.py
+++ engineers.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import Optional
+from db import cursor
 
 
 @dataclass
@@ -8,3 +9,11 @@ class Engineer:
     FirstName: str
     LastName: str
     Rate: float
+
+    def setrate(self, rate: float) -> None:
+        '''Set the engineer's rate.'''
+        db.execute("UPDATE Engineers SET Rate=%s WHERE EngineerID=%s",
+                   (rate, self.EngineerID))
+        db.commit()
+        self.Rate = rate
+        print(f'Engineer {self.FirstName} {self.LastName} rate set to {rate}')
