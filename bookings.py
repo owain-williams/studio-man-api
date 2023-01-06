ef5ead3a33f292e3cb533ede0df319ef67ae093f
Owain Williams <owain@andersdigital.com>
2023-01-06 09:32:17 +0000

    add bookings commit function

--- bookings.py
+++ bookings.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 from typing import Optional
 from decimal import Decimal
+from db import db
+
+cursor = db.cursor()
 
 
 @dataclass
@@ -8,8 +11,22 @@ class Booking:
     BookingID: Optional[int]
     Client: int
     Room: int
-    Engineers: list[int]
-    DaySessions: list[int]
+    Engineers: Optional[list[int]]
+    DaySessions: Optional[list[int]]
     InitialCost: Decimal
     DiscountAmount: Decimal
     ActualCost: Decimal
+
+    def commit(self) -> None:
+        if self.BookingID is None:
+            cursor.execute("INSERT INTO Bookings (Client, Room, Engineers, DaySessions, InitialCost, DiscountAmount, ActualCost) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                            (self.Client, self.Room, self.Engineers, self.DaySessions, self.InitialCost, self.DiscountAmount, self.ActualCost))
+            db.commit()
+            self.BookingID = cursor.lastrowid
+            print(f'Booking created. ID: {self.BookingID}')        
+        
+    
+    def cancel(self):
+        pass
+    
+    
